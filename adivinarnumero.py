0b8a8b9a572a8eb5f072d1bad472be91feb67e70
Jacobo24 <jaccalgar@gmail.com>
2022-11-17 21:35:55 +0100

    qq

--- adivinarnumero.py
+++ adivinarnumero.py
@@ -1,34 +1,26 @@
-print("Introduzca el número a adivinar")
-while True:
-    numero = input("Introduzca un número entre 0 y 99 incluidos: ")
-
-    try:
-        numero = int(numero)
-    except:
-        pass
+def menu():
+    print("Adivina un numero")
+    print("Nivel1\nNivel2\nNivel3\nNivel4")
+    nivel = input("Ingresa el nivel: ")
+    print(nivel)
+    if nivel== "Nivel1":
+        juego1(0,100)
+    elif nivel == "Nivel2":
+        juego1(0,1000)
+    elif nivel == "Nivel3":
+        juego1(0,10000)
+    elif nivel == "Nivel4":
+        juego1(0,100000)
     else:
-        if 0 <= numero <= 99:
-            break
+        return menu()
+def numeroaleatorio(minimo, maximo):
+    import random
+    numeroaleatorio = random.randint(minimo, maximo)
+    return numeroaleatorio
+def juego1(minimo, maximo):
+    naleatorio = numeroaleatorio(minimo, maximo)
+    print(naleatorio)
+    numero = input()
 
 
-print("Intente encontrar el número a adivinar")
-while True:
-
-    while True:
-        intento = input("Introduzca un número entre 0 y 99 incluidos: ")
-
-        try:
-            intento = int(intento)
-        except:
-            pass
-        else:
-            if 0 <= intento <= 99:
-                break
-
-    if intento < numero:
-        print("Demasiado pequeño")
-    elif intento > numero:
-        print("Demasiado grande")
-    else:
-        print("¡Ha ganado!")
-        break
+menu()
